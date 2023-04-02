9810e390fab040d4134011e650fc10f175d831d8
Kacper Tomczyk <46459137+Skinnero@users.noreply.github.com>
2023-04-02 18:07:29 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, url_for
+from dotenv import load_dotenv
+from util import json_response
+import queries
+
+app = Flask(__name__)
+load_dotenv()
+
+@app.route("/")
+def index():
+    """
+    This is a one-pager which shows all the boards and cards
+    """
+    return render_template('index.html')
+
+
+@app.route("/api/boards")
+@json_response
+def get_boards():
+    """
+    All the boards
+    """
+    return queries.get_boards()
+
+
+@app.route("/api/boards/<int:board_id>/cards/")
+@json_response
+def get_cards_for_board(board_id: int):
+    """
+    All cards that belongs to a board
+    :param board_id: id of the parent board
+    """
+    return queries.get_cards_for_board(board_id)
+
+
+def main():
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    main()
