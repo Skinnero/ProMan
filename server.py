7fc3a00d6e2054c140e07d5a121a5fc6cf6a5c30
Kacper Tomczyk <46459137+Skinnero@users.noreply.github.com>
2023-04-26 10:47:38 +0200

    addig web socket events to js and server

--- server.py
+++ server.py
@@ -1,46 +1,49 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
+from flask_socketio import SocketIO
 from datetime import timedelta
+from dotenv import load_dotenv
+from os import environ
+
 from route.boards import api_boards
 from route.columns import api_columns
 from route.cards import api_cards
 from route.users import api_users
 from data_handler.boards import get_all
 
+load_dotenv()
+
 app = Flask(__name__)
 app.register_blueprint(api_boards)
 app.register_blueprint(api_columns)
 app.register_blueprint(api_cards)
 app.register_blueprint(api_users)
-app.config['JWT_SECRET_KEY'] = b'xD'
-app.config['SECRET_KEY'] = b'-_-'
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+app.config['JWT_SECRET_KEY'] = environ.get('JWT_SECRET_KEY')
+
 app.config['JWT_TOKEN_LOCATION'] = ["cookies"]
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=5)
+
+socketio = SocketIO(app, cors_allowed_origins='*')
 jwt = JWTManager(app)
+import web_sockets
 
 
 @app.route("/")
+@jwt_required(optional=True)
 def index():
     """Render main page
 
     Returns:
         render_template: index.html
     """
-    return render_template('index.html', boards=get_all())
-
-
-@app.route('/private', methods=["GET"])
-@jwt_required(optional=True)
-def user_room():
-    current_user = get_jwt_identity()
-    print(current_user)
-    return jsonify(logged_in_as=current_user), 200
+    return render_template('index.html', boards=get_all(), user=get_jwt_identity())
 
 
 def main():
     """Main function
     """
-    app.run(debug=True)
+    socketio.run(app, debug=True)
 
 
 if __name__ == '__main__':
